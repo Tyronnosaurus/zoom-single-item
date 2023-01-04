4656ee63a35946f7ddf96e92c3aa1e4c15f11ee7
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2023-01-04 11:39:29 +0100

    Deleted obsolete code

--- ZZZ_old/action_button.js
+++
@@ -1,9 +0,0 @@
-
-//When user clicks on the extension's Action button (the one on the browser's top left), open the options page
-//Must fill browser_action in manifest.json for the button to appear
-
-function handleClick() {
-  browser.runtime.openOptionsPage();
-}
-
-browser.browserAction.onClicked.addListener(handleClick);
